74d35feba3248eb697bea60e160b48cc51ae3142
Morpheus <57977077+dledbetter123@users.noreply.github.com>
2024-02-15 01:06:31 -0500

    updated wesbsite hosted zone for www. subdomain

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -9,7 +9,7 @@ echo "Going to the build directory..."
 cd build
 
 echo "Deploying to S3 bucket..."
-aws s3 sync . s3://nsbeumbc.com --delete
+aws s3 sync . s3://www.nsbeumbc.com --delete
 
 # echo "Invalidating CloudFront cache..."
 # aws cloudfront create-invalidation --distribution-id YOUR_DISTRIBUTION_ID --paths "/*"
